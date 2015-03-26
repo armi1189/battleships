6f02bf98f070c1196e52edc6d108b657327d2612
armi1189 <armi1189@gmail.com>
2015-03-26 21:53:27 +0000

    Board can place a ship horizontally

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,10 @@ class Board
   attr_reader :grid
 
   DEFAULT_SIZE = 10
+  DEFAULT_CELL = String
   def initialize(options = {})
     @size = options.fetch(:size, DEFAULT_SIZE)
+    @cell = options.fetch(:cell, DEFAULT_CELL)
     @grid = {}
     initialize_grid @size
   end
@@ -11,8 +13,20 @@ class Board
   def initialize_grid size
     ('A'..(size + 64).chr).each do |letter|
       (1..size).each do |number|
-        grid[("#{letter}#{number}").to_sym] = :cell
+        grid[("#{letter}#{number}").to_sym] = @cell.new
       end
     end
+  end
+
+  def place ship, coord
+    get_coords(ship.size, coord).each do |coordinate|
+      grid[coordinate].container = ship
+    end
+  end
+
+  def get_coords size, coord
+    coords = [coord]
+    (size - 1).times { coords << coords.last.next }
+    coords
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,15 +1,33 @@
 require 'board'
 
 describe Board do
-  let(:board) { Board.new }
+  let(:board) { Board.new cell: cell_class }
+  let(:cell_class) { double :cell_class, new: cell }
+  let(:cell) { double :cell }
+  let(:host_cell) { double :host_cell }
+  let(:ship) { double :ship, size: 2 }
+
   context 'when created' do
     it 'can have a default size of 10x10' do
       expect(board.grid.length).to eq 100
     end
 
     it 'can have a variable size' do
-      board = Board.new size: 5
+      board = Board.new size: 5, cell: cell_class
       expect(board.grid.length).to eq 25
+    end
+  end
+
+  context 'after been created' do
+    it 'can work out coordinates for a size' do
+      expect(board.get_coords 2, :A1).to eq [:A1, :A2]
+    end
+
+    it 'can place a size 2 ship horizontally' do
+      board.grid[:A1] = host_cell
+      board.grid[:A2] = host_cell
+      expect(host_cell).to receive(:container=).with(ship).exactly(2).times
+      board.place ship, :A1
     end
   end
 end
